7c41511466f006b74e94401bbbbde73aa0756982
Mozefoka <musa.hutiev95@gmail.com>
2024-01-07 15:27:06 +0300

    Fix: video files

--- src/scripts/components/video.js
+++ src/scripts/components/video.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const playBtn = document.querySelector('.play-btn');
-const video = document.querySelector('.video');
-const videoFile = document.querySelector('.video-file');
-const cross = document.querySelector('.video-cross');
+const playBtn = document.querySelector('.specs__play-btn');
+const video = document.querySelector('.specs__video');
+const videoFile = document.querySelector('.specs__video-file');
+const cross = document.querySelector('.specs__video-cross');
 
 playBtn.addEventListener ('click', function() {
-	if (!video.classList.contains('video--active')) {
-		video.classList.add('video--active');
+	if (!video.classList.contains('specs__video--active')) {
+		video.classList.add('specs__video--active');
 	}
 });
 
 cross.addEventListener('click', function() {
 	videoFile.pause();
 	videoFile.currentTime = 0;
-	video.classList.remove('video--active');
+	video.classList.remove('specs__video--active');
  });
