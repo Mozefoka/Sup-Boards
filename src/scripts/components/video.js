7b77dc04fdbd6c25f508b91dea7347b42277c594
Mozefoka <musa.hutiev95@gmail.com>
2024-01-07 15:46:45 +0300

    fix: names

--- src/scripts/components/video.js
+++ src/scripts/components/video.js
@@ -3,7 +3,7 @@
 const playBtn = document.querySelector('.specs__play-btn');
 const video = document.querySelector('.specs__video');
 const videoFile = document.querySelector('.specs__video-file');
-const close = document.querySelector('.specs__video-close');
+const cross = document.querySelector('.specs__video-cross');
 
 playBtn.addEventListener ('click', function() {
 	if (!video.classList.contains('specs__video--active')) {
@@ -11,7 +11,7 @@ playBtn.addEventListener ('click', function() {
 	}
 });
 
-close.addEventListener('click', function() {
+cross.addEventListener('click', function() {
 	videoFile.pause();
 	videoFile.currentTime = 0;
 	video.classList.remove('specs__video--active');
