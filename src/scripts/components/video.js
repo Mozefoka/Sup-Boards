dabda0adc8de16d9d88b687da52fcf8e186477e5
Mozefoka <musa.hutiev95@gmail.com>
2024-01-07 15:39:09 +0300

    fix

--- src/scripts/components/video.js
+++ src/scripts/components/video.js
@@ -3,7 +3,7 @@
 const playBtn = document.querySelector('.specs__play-btn');
 const video = document.querySelector('.specs__video');
 const videoFile = document.querySelector('.specs__video-file');
-const cross = document.querySelector('.specs__video-cross');
+const close = document.querySelector('.specs__video-close');
 
 playBtn.addEventListener ('click', function() {
 	if (!video.classList.contains('specs__video--active')) {
@@ -11,7 +11,7 @@ playBtn.addEventListener ('click', function() {
 	}
 });
 
-cross.addEventListener('click', function() {
+close.addEventListener('click', function() {
 	videoFile.pause();
 	videoFile.currentTime = 0;
 	video.classList.remove('specs__video--active');
